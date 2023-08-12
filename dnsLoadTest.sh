082c157c3d295530592da36c4ed13b9110b33709
Alex_Yin <36754105+yinoa@users.noreply.github.com>
2023-08-12 12:59:25 +0000

    Update dnsLoadTest.sh

--- dnsLoadTest.sh
+++ dnsLoadTest.sh
@@ -9,6 +9,6 @@ do
   result=$(nslookup ${dnsname} ${dnsserver} | awk '!a[$0]++' | awk '!a[$0]++' |sed 's/\;;//'|egrep 'timed|reached'|xargs)
   ipAddr=$(ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}')
   if [$result]; then
-  echo "$(date -u +'%F %H:%M:%S.%3N') ${HOSTNAME} ${ipAddr} ${dnsname} ${result}" >> /filestorage/01/$HOSTNAME.txt
+  echo "$(date -u +'%F %H:%M:%S.%3N') | ${HOSTNAME} | ${ipAddr} | ${dnsname} | ${result}" >> /filestorage/01/$HOSTNAME.txt
   fi
 done
