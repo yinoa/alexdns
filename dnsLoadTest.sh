e080710a3de1ab195533a444430a052106aabdfb
Alex_Yin <36754105+yinoa@users.noreply.github.com>
2023-08-12 12:55:37 +0000

    Update dnsLoadTest.sh

--- dnsLoadTest.sh
+++ dnsLoadTest.sh
@@ -8,5 +8,7 @@ while true
 do
   result=$(nslookup ${dnsname} ${dnsserver} | awk '!a[$0]++' | awk '!a[$0]++' |sed 's/\;;//'|egrep 'timed|reached'|xargs)
   ipAddr=$(ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}')
-  echo "$(date -u +'%F %H:%M:%S.%3N') ${HOSTNAME} ${ipAddr} ${dnsname} ${result}" | grep "timed out" >> /filestorage/01/$HOSTNAME.txt
+  if [$result]; then
+  echo "$(date -u +'%F %H:%M:%S.%3N') ${HOSTNAME} ${ipAddr} ${dnsname} ${result}" >> /filestorage/01/$HOSTNAME.txt
+  fi
 done
