b0032b64a8a0710586f1b2598ff7384bf7fbbc6f
Alex_Yin <36754105+yinoa@users.noreply.github.com>
2024-01-06 01:53:48 +0000

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Example: ./dnsLoadTest.sh www.bing.com 168.63.129.16
+
+dnsname=$1
+dnsserver=$2
+
+while true
+do
+  result=$(nslookup ${dnsname} ${dnsserver} | awk '!a[$0]++' | awk '!a[$0]++' |sed 's/\;;//'|egrep 'timed|reached'|xargs)
+  ipAddr=$(ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}')
+  if [ -n "$result" ]; then
+  echo "$(date -u +'%F %H:%M:%S.%3N') | ${HOSTNAME} | ${ipAddr} | ${dnsname} | ${result}" >> /filestorage/01/$HOSTNAME.txt
+  fi
+done
