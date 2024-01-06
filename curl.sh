b7e2aeb14071aca79a8514c98b274e3455dc6cd9
Alex_Yin <36754105+yinoa@users.noreply.github.com>
2024-01-06 13:40:07 +0000

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Example: ./curl.sh
 
-dnsname=$1
-dnsserver=$2
-
-while true
+for i in {1..5}
 do
-  result=$(nslookup ${dnsname} ${dnsserver} | awk '!a[$0]++' | awk '!a[$0]++' |sed 's/\;;//'|egrep 'timed|reached'|xargs)
-  ipAddr=$(ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}')
-  if [ -n "$result" ]; then
-  echo "$(date -u +'%F %H:%M:%S.%3N') | ${HOSTNAME} | ${ipAddr} | ${dnsname} | ${result}" >> /filestorage/01/$HOSTNAME.txt
-  fi
+    (
+        while true
+        do
+            curl -I https://alexapp.alexazure.com --resolve alexapp.alexazure.com:443:13.107.226.46
+            sleep 1
+        done
+    ) &
 done
+
+wait
